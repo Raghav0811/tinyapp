75a2d58d9935cb2244499bb880d9e05d177d5eb3
vagrant <raghavbpdshrivastav@gmail.com>
2020-05-12 23:03:55 +0000

    added POST for delete url functionality

--- express_server.js
+++ express_server.js
@@ -36,7 +36,10 @@ app.get("/urls", (req, res) => {
 app.post("urls", (req, res) => {
   console.log(req.body);
   res.send("OK");
+  res.redirect(`/urls/${shortURL}`);
 });
+
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -54,4 +57,30 @@ app.get("/urls/:shortURL", (req, res) => {
   console.log(req.params.shortURL)
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+
+
+
+// /u/:shortURL -- access the actual link (longURL)
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
+});
+
+
+app.post("/urls/:shortURL/delete", (req,res) => {
+  const shortURL = req.params.shortURL;
+  if (req.session.user_id === urlDatabase[shortURL].userID) {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  } else {
+    let templateVars = {
+      status: 401,
+      message: 'You are not allowed to delete that TinyURL',
+      user: users[req.session.user_id]
+    }
+    res.status(401);
+    res.render("urls_error", templateVars);
+  }
 });
