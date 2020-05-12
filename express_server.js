325516d1cd4d87a63c14aa04933d925d914d0c45
vagrant <raghavbpdshrivastav@gmail.com>
2020-05-12 00:33:06 +0000

    adding HTML carcass! to urls index

--- express_server.js
+++ express_server.js
@@ -13,6 +13,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
